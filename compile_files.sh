8c837f57c4d3b1f761d5f133f827181a3495463d
dron <dr_dronych@mail.ru>
2021-06-24 07:55:14 +0200

    implemented base version

---
+++ compile_files.sh
@@ -0,0 +1,24 @@
+dir=$1
+
+echo "Compiling in: $dir..."
+
+
+files=$(ls $dir)
+compiled="compiled.csv"
+
+touch $compiled
+
+i=0
+
+for file in $files; do
+	if [[ "$i" -ne 0 ]]; then
+		tail -n +2 $dir/$file >> $compiled
+	else
+		tail -n +0 $dir/$file >> $compiled
+	fi
+	
+	(( i++ ))
+	echo "Processing file #: $i -- $dir/$file"
+done
+
+echo "Created $compiled......Done"
